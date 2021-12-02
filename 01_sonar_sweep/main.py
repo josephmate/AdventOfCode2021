b680ac181bbae6c4a0f6a4990a5780b246280095
Joseph <mate.joseph@gmail.com>
2021-12-01 20:59:31 -0500

    part 2 complete

--- 01_sonar_sweep/main.py
+++ 01_sonar_sweep/main.py
@@ -14,3 +14,12 @@ for current_depth in input:
         increments += 1
     prev_depth = current_depth
 print(increments)
+
+increments = 0
+prev_sum = input[0] + input[1] + input[2]
+for i in range(3, len(input)):
+    new_sum = prev_sum + input[i] - input[i-3]
+    if new_sum > prev_sum:
+        increments += 1
+    prev_sum = new_sum
+print(increments)

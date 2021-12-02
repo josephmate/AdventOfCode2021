c385022ca153b74111f623c8e2351fb3600cd3ec
Joseph <mate.joseph@gmail.com>
2021-12-02 17:06:22 -0500

    part 2 complete

--- 02_dive/main.py
+++ 02_dive/main.py
@@ -16,5 +16,17 @@ for (direction, magnitude) in input:
         current_depth += magnitude
     elif direction == "up":
         current_depth -= magnitude
-
 print(f"current_posn={current_posn} current_depth={current_depth} current_posn*current_depth={current_posn*current_depth}")
+
+aim = 0
+current_posn = 0
+current_depth = 0
+for (direction, magnitude) in input:
+    if direction == "forward":
+        current_posn += magnitude
+        current_depth += aim*magnitude
+    elif direction == "down":
+        aim += magnitude
+    elif direction == "up":
+        aim -= magnitude
+print(f"aim={aim} current_posn={current_posn} current_depth={current_depth} current_posn*current_depth={current_posn*current_depth}")

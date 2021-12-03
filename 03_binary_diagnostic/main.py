aec974ca8a08fd59baf0eab5ec08a288684de0dc
Joseph <mate.joseph@gmail.com>
2021-12-03 12:39:53 -0500

    got the oxygen generator rating

--- 03_binary_diagnostic/main.py
+++ 03_binary_diagnostic/main.py
@@ -41,3 +41,38 @@ gamma = binary_array_to_int(gamma_binary)
 epsilon = binary_array_to_int(epsilon_binary)
 power = gamma*epsilon
 print(f"gamma={gamma} epsilon={epsilon} power={power}")
+
+
+potential_generator_rating = input.copy()
+current_posn = 0
+while len(potential_generator_rating) > 1:
+    counts = [0, 0]
+    for binary_number in potential_generator_rating:
+        counts[int(binary_number[current_posn])] += 1
+    
+    criteria = '0'
+    if counts[1] >= counts[0]:
+        criteria = '1'
+    
+    potential_generator_rating = list(
+        filter(
+            lambda binary_number: binary_number[current_posn] == criteria,
+            potential_generator_rating
+        )
+    )
+    current_posn += 1
+
+
+def binary_str_to_int(binary_str):
+    binary_array = []
+    for binary_digit_str in binary_str:
+        binary_array.append(int(binary_digit_str))
+    return binary_array_to_int(binary_array)
+
+print(potential_generator_rating)
+print(binary_str_to_int(potential_generator_rating[0]))
+generator_rating = binary_str_to_int(potential_generator_rating[0])
+
+
+
+print(f"generator_rating={generator_rating}")

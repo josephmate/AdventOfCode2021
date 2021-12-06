3d67530b478398b24cbec42a56240891b0459492
Joseph <mate.joseph@gmail.com>
2021-12-06 09:06:06 -0500

    day 6 part 1 complete

--- 06_latternfish/main.py
+++ 06_latternfish/main.py
@@ -23,3 +23,19 @@ for i in range(0, 8+1):
 for fish_timer in input:
     fish_timers[fish_timer] += 1
 print(fish_timers)
+
+
+for day in range(1, 80+1):
+    new_fish_timers = []
+    for i in range(0, 8+1):
+        new_fish_timers.append(0)
+    
+    for i in range(1, 8+1):
+        new_fish_timers[i-1] = fish_timers[i]
+    
+    new_fish_timers[8] = fish_timers[0]
+    new_fish_timers[6] += fish_timers[0]
+
+    fish_timers = new_fish_timers
+
+print(sum(fish_timers))

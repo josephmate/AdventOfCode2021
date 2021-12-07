6a58b653d44a76564e615cfa1a4c78ec265bcd7d
Joseph <mate.joseph@gmail.com>
2021-12-07 08:48:10 -0500

    O(N^3) isn't going to work for part2. need to calculate the fuel consumption in constant instead of linear

--- 07_the_treachery_of_whales/main.py
+++ 07_the_treachery_of_whales/main.py
@@ -33,3 +33,26 @@ for posn in range(min_posn, max_posn+1):
         min_fuel = fuel
         target_posn = posn
 print(f"min_fuel={min_fuel} target_posn={target_posn}")
+
+def calc_movement(posn, target_posn):
+    delta = abs(posn - target_posn)
+    sum = 0
+    for i in range(1, delta+1):
+        sum += i
+    return sum
+
+def calc_expensive_fuel(target_posn, posns) :
+    sum = 0
+    for posn in posns:
+        sum += calc_movement(posn, target_posn)
+    return sum
+
+min_fuel = 1000000000
+target_posn = -1
+for posn in range(min_posn, max_posn+1):
+    fuel = calc_expensive_fuel(posn, input)
+    if fuel < min_fuel:
+        min_fuel = fuel
+        target_posn = posn
+print(f"min_fuel={min_fuel} target_posn={target_posn}")
+

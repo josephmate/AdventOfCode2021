77b31312d05433f3e26b5857d5a737e9abd38ad0
Joseph <mate.joseph@gmail.com>
2021-12-12 10:42:18 -0500

    intialized day 11

---
+++ 11_dumbo_octopus/main.py
@@ -0,0 +1,11 @@
+import sys
+import re
+from queue import PriorityQueue
+from queue import LifoQueue as Stack
+
+input = list(
+    map(lambda line : line.rstrip(),
+    sys.stdin.readlines()
+    ))
+
+print(input)

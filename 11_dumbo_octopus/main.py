62301dab372e4d2d19e10183aad038d39dde44d5
Joseph <mate.joseph@gmail.com>
2021-12-12 11:32:00 -0500

    Day 11 part 2 complete

--- 11_dumbo_octopus/main.py
+++ 11_dumbo_octopus/main.py
@@ -2,6 +2,7 @@ import sys
 import re
 from queue import PriorityQueue
 from queue import LifoQueue as Stack
+import copy
 
 input = list(
     map(lambda line : list(map(lambda col: int(col), line)),
@@ -76,4 +77,64 @@ def simulate(steps, octopus_map):
     return num_of_flashes
 
 print('sample.txt 1656')
-print(simulate(100, input.copy()))
+print(simulate(100, copy.deepcopy(input)))
+
+def all_flash_step(octopus_map):
+    flashed = []
+    for r in range(0, num_rows):
+        current_row = []
+        for c in range(0, num_cols):
+            current_row.append(False)
+        flashed.append(current_row)
+
+    something_flashed = True
+    while something_flashed:
+        something_flashed = False
+        for r in range(0, num_rows):
+            for c in range(0, num_rows):
+                if octopus_map[r][c] > 9 and not flashed[r][c]:
+                    flashed[r][c] = True
+                    something_flashed = True
+                    if r - 1 >= 0:
+                        octopus_map[r-1][c] = octopus_map[r-1][c] + 1
+                    if r + 1 < num_rows:
+                        octopus_map[r+1][c] = octopus_map[r+1][c] + 1
+                    if c - 1 >= 0:
+                        octopus_map[r][c-1] = octopus_map[r][c-1] + 1
+                    if c + 1 < num_cols:
+                        octopus_map[r][c+1] = octopus_map[r][c+1] + 1
+                    # diagonals
+                    if r - 1 >= 0 and c - 1 >= 0:
+                        octopus_map[r-1][c-1] = octopus_map[r-1][c-1] + 1
+                    if r - 1 >= 0 and c + 1 < num_rows:
+                        octopus_map[r-1][c+1] = octopus_map[r-1][c+1] + 1
+                    if r + 1 < num_rows and c - 1 >= 0:
+                        octopus_map[r+1][c-1] = octopus_map[r+1][c-1] + 1
+                    if r + 1 < num_rows and c + 1 < num_rows:
+                        octopus_map[r+1][c+1] = octopus_map[r+1][c+1] + 1
+
+    # reset all values > 9 to 0
+    for r in range(0, num_rows):
+        for c in range(0, num_rows):
+            if octopus_map[r][c] > 9:
+                octopus_map[r][c] = 0
+
+    for r in range(0, num_rows):
+        for c in range(0, num_rows):
+            if not flashed[r][c]:
+                return False
+    return True
+    
+
+def all_flash(octopus_map):
+    for step in range(1, 1_000_000_000):
+        # increment everything
+        for r in range(0, num_rows):
+            for c in range(0, num_cols):
+                octopus_map[r][c] = octopus_map[r][c]+1
+        
+        if all_flash_step(octopus_map):
+            return step
+
+print("sample.txt 195")
+print(all_flash(copy.deepcopy(input)))

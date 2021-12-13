3951d957b16fb38dc06b9efeca83b697dd5696ba
Joseph <mate.joseph@gmail.com>
2021-12-13 08:58:20 -0500

    input parsed

--- 13_transparent_origami/main.py
+++ 13_transparent_origami/main.py
@@ -4,10 +4,26 @@ from queue import PriorityQueue
 from queue import LifoQueue as Stack
 from queue import Queue
 
-
+# 1,2
+coord_pattern = r'(\d+),(\d+)'
+# fold along y=7
+fold_pattern = r'fold along (\w+)=(\d+)'
 input = list(
     map(lambda line : line.rstrip(),
     sys.stdin.readlines()
     ))
 
 print(input)
+
+coords = list(
+    map(lambda match: ( int(match.group(1)), int(match.group(2))),
+    filter(lambda match: not match == None,
+    map(lambda line: re.search(coord_pattern, line),
+    input))))
+print(coords)
+folds = list(
+    map(lambda match: (match.group(1), int(match.group(2))),
+    filter(lambda match: not match == None,
+    map(lambda line : re.search(fold_pattern, line),
+    input))))
+print(folds)

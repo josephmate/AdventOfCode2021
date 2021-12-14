344e2ee4767656a4741d036204d7a4864ce75b69
Joseph <mate.joseph@gmail.com>
2021-12-14 08:36:24 -0500

    parsed

--- 14_extended_polymerization/main.py
+++ 14_extended_polymerization/main.py
@@ -10,3 +10,15 @@ input = list(
     ))
 
 print(input)
+print(input[0])
+print(input[2:len(input)])
+
+pattern = r'(\w+) -> (\w+)'
+polymer_template = input[0]
+insertion_rules = list(
+    map(lambda match: (match.group(1), match.group(2)),
+    map(lambda line: re.search(pattern, line),
+    input[2:len(input)])))
+
+print(pattern)
+print(insertion_rules)

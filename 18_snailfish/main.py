2cfd1a3f39e001f213a85b3eb84d4187259e0e56
Joseph <mate.joseph@gmail.com>
2021-12-18 07:38:56 -0500

    Parsing the input as json so we dont need to write our own parser

--- 18_snailfish/main.py
+++ 18_snailfish/main.py
@@ -3,11 +3,14 @@ import re
 from queue import PriorityQueue
 from queue import LifoQueue as Stack
 from queue import Queue
+import json
 
 input = list(
+    map(lambda line : json.loads(line),
     map(lambda line : line.rstrip(),
     sys.stdin.readlines()
-    ))
+    )))
 
 print(input)
+
 

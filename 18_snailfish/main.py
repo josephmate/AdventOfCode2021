6a16d933579282a8abdbf22b1aa824c9656c5e70
Joseph <mate.joseph@gmail.com>
2021-12-18 21:40:40 -0500

    part 1 complete

--- 18_snailfish/main.py
+++ 18_snailfish/main.py
@@ -228,10 +228,6 @@ actual = sn_add( [[[[4,3],4],4],[7,[[8,4],9]]] , [1,1] )
 print(f"Expected: {expected}")
 print(f"Actual:   {actual}")
 assert(f"{expected}" == f"{actual}")
-exit()
-
-print([[[[0,7],4],[[7,8],[6,0]]],[8,1]])
-print(sn_add([[[[4,3],4],4],[7,[[8,4],9]]], [1,1]))
 print("")
 
 def sn_sum(sns):
@@ -240,29 +236,132 @@ def sn_sum(sns):
         sum = sn_add(sum, sns[i])
     return sum
 
-print([[[[1,1],[2,2]],[3,3]],[4,4]])
-print(sn_sum([
-[1,1],
-[2,2],
-[3,3],
-[4,4],
-]))
+
+expected = [[[[1,1],[2,2]],[3,3]],[4,4]]
+actual = sn_sum([
+    [1,1],
+    [2,2],
+    [3,3],
+    [4,4],
+])
+print(f"Expected: {expected}")
+print(f"Actual:   {actual}")
+assert(f"{expected}" == f"{actual}")
 print("")
-print([[[[3,0],[5,3]],[4,4]],[5,5]])
-print(sn_sum([
-[1,1],
-[2,2],
-[3,3],
-[4,4],
-[5,5],
-]))
+
+expected = [[[[3,0],[5,3]],[4,4]],[5,5]]
+actual = sn_sum([
+    [1,1],
+    [2,2],
+    [3,3],
+    [4,4],
+    [5,5],
+])
+print(f"Expected: {expected}")
+print(f"Actual:   {actual}")
+assert(f"{expected}" == f"{actual}")
 print("")
-print([[[[5,0],[7,4]],[5,5]],[6,6]])
-print(sn_sum([
-[1,1],
-[2,2],
-[3,3],
-[5,5],
-[6,6],
-]))
+
+
+expected = [[[[5,0],[7,4]],[5,5]],[6,6]]
+actual = sn_sum([
+    [1,1],
+    [2,2],
+    [3,3],
+    [4,4],
+    [5,5],
+    [6,6],
+])
+print(f"Expected: {expected}")
+print(f"Actual:   {actual}")
+assert(f"{expected}" == f"{actual}")
 print("")
+
+expected = [[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]
+actual = sn_sum([
+    [[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]],
+    [7,[[[3,7],[4,3]],[[6,3],[8,8]]]],
+    [[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]],
+    [[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]],
+    [7,[5,[[3,8],[1,4]]]],
+    [[2,[2,2]],[8,[8,1]]],
+    [2,9],
+    [1,[[[9,3],9],[[9,0],[0,7]]]],
+    [[[5,[7,4]],7],1],
+    [[[[4,2],2],6],[8,7]],
+])
+print(f"Expected: {expected}")
+print(f"Actual:   {actual}")
+assert(f"{expected}" == f"{actual}")
+print("")
+
+def sn_magnitude(sn):
+    if isinstance(sn, int):
+        return sn
+    else:
+        # The magnitude of a pair is 
+        # 3 times the magnitude of its left element
+        # plus 2 times the magnitude of its right element.
+        return (3 * sn_magnitude(sn[0])) + (2 * sn_magnitude(sn[1]))
+
+expected = 143
+actual = sn_magnitude([[1,2],[[3,4],5]])
+print(f"Expected: {expected}")
+print(f"Actual:   {actual}")
+assert(f"{expected}" == f"{actual}")
+print("")
+
+expected = 1384
+actual = sn_magnitude([[[[0,7],4],[[7,8],[6,0]]],[8,1]])
+print(f"Expected: {expected}")
+print(f"Actual:   {actual}")
+assert(f"{expected}" == f"{actual}")
+print("")
+
+expected = 445
+actual = sn_magnitude([[[[1,1],[2,2]],[3,3]],[4,4]])
+print(f"Expected: {expected}")
+print(f"Actual:   {actual}")
+assert(f"{expected}" == f"{actual}")
+print("")
+
+expected = 791
+actual = sn_magnitude([[[[3,0],[5,3]],[4,4]],[5,5]])
+print(f"Expected: {expected}")
+print(f"Actual:   {actual}")
+assert(f"{expected}" == f"{actual}")
+print("")
+
+expected = 1137
+actual = sn_magnitude([[[[5,0],[7,4]],[5,5]],[6,6]])
+print(f"Expected: {expected}")
+print(f"Actual:   {actual}")
+assert(f"{expected}" == f"{actual}")
+print("")
+
+expected = 3488
+actual = sn_magnitude([[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]])
+print(f"Expected: {expected}")
+print(f"Actual:   {actual}")
+assert(f"{expected}" == f"{actual}")
+print("")
+
+expected = 4140
+actual = sn_magnitude(sn_sum([
+    [[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]],
+    [[[5,[2,8]],4],[5,[[9,9],0]]],
+    [6,[[[6,2],[5,6]],[[7,6],[4,7]]]],
+    [[[6,[0,7]],[0,9]],[4,[9,[9,0]]]],
+    [[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]],
+    [[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]],
+    [[[[5,4],[7,7]],8],[[8,3],8]],
+    [[9,3],[[9,9],[6,[4,9]]]],
+    [[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]],
+    [[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]],
+]))
+print(f"Expected: {expected}")
+print(f"Actual:   {actual}")
+assert(f"{expected}" == f"{actual}")
+print("")
+
+print(sn_magnitude(sn_sum(input)))

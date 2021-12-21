bdd54045cc8062798a619c2ad30cc3ef2fbc140e
Joseph <mate.joseph@gmail.com>
2021-12-21 12:18:08 -0500

    Day 21 init.

---
+++ 21_dirac_dice/main.py
@@ -0,0 +1,11 @@
+import sys
+import re
+from queue import PriorityQueue
+from queue import LifoQueue as Stack
+from queue import Queue
+import json
+import copy
+from collections import deque
+
+sample = [4,8]
+input = [8,9]

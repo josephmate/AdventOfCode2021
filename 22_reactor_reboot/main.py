47b36c7e5556a03ca5fc6128d061ec32bb34a1e1
Joseph <mate.joseph@gmail.com>
2021-12-22 11:23:44 -0500

    part 1 complete

--- 22_reactor_reboot/main.py
+++ 22_reactor_reboot/main.py
@@ -16,10 +16,11 @@ def parse_file(filename):
         for line in r:
             line = line.rstrip()
             match = re.search(pattern, line)
-            lines.append((match.group(1),
-                match.group(2), match.group(3),
-                match.group(4), match.group(5),
-                match.group(6), match.group(7)
+            is_on = match.group(1) == "on"
+            lines.append((is_on,
+                int(match.group(2)), int(match.group(3)),
+                int(match.group(4)), int(match.group(5)),
+                int(match.group(6)), int(match.group(7))
                 ))
     return lines
 
@@ -27,4 +28,31 @@ sample_small = parse_file('sample_small.txt')
 sample_large = parse_file('sample_large.txt')
 input = parse_file('input.txt')
 
-print(sample_small)
+def solve_small(steps):
+    on_cubes = set()
+
+    for is_on, x1, x2, y1, y2, z1, z2 in steps:
+        if (
+            x1 >= -50 and x1 <= 50 and
+            x2 >= -50 and x2 <= 50 and
+            y1 >= -50 and y1 <= 50 and
+            y2 >= -50 and y2 <= 50 and
+            z1 >= -50 and z1 <= 50 and
+            z2 >= -50 and z2 <= 50
+        ):
+            for x in range(x1, x2+1):
+                for y in range(y1, y2+1):
+                    for z in range(z1, z2+1):
+                        if is_on:
+                            on_cubes.add((x,y,z))
+                        elif (x,y,z) in on_cubes:
+                            on_cubes.remove((x,y,z))
+
+    return len(on_cubes)
+
+print("39")
+print(solve_small(sample_small))
+print("590784")
+print(solve_small(sample_large))
+print("input soln")
+print(solve_small(input))

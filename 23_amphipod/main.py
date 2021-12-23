94b5f1e3a59c4f2fc796aa5ba8cb98fd4618f15c
Joseph <mate.joseph@gmail.com>
2021-12-23 17:57:37 -0500

    Slowly adding depth to all the methods

--- 23_amphipod/main.py
+++ 23_amphipod/main.py
@@ -10,6 +10,8 @@ from typing import OrderedDict
 import math
 from heapq import *
 
+debug = True
+
 # on x=10..12,y=10..12,z=10..12
 # off x=9..11,y=9..11,z=9..11
 pattern = r'(\w+) x=([0-9-]+)..([0-9-]+),y=([0-9-]+)..([0-9-]+),z=([0-9-]+)..([0-9-]+)'
@@ -39,12 +41,20 @@ cave_map = {
     (1,11): '.',
     (2,3): 'A',
     (3,3): 'A',
+    (4,3): 'A',
+    (5,3): 'A',
     (2,5): 'B',
     (3,5): 'B',
+    (4,5): 'B',
+    (5,5): 'B',
     (2,7): 'C',
     (3,7): 'C',
+    (4,7): 'C',
+    (5,7): 'C',
     (2,9): 'D',
     (3,9): 'D',
+    (4,9): 'D',
+    (5,9): 'D',
 }
 sample = parse_file('sample.txt')
 input = parse_file('input.txt')
@@ -55,6 +65,19 @@ print()
 print("\n".join(map(lambda pair: str(pair), input.items())))
 print()
 
+
+def print_map(amphipod_map, depth):
+    for r in range (1,1+depth+1):
+        for c in range (1,11+1):
+            if (r,c) in amphipod_map:
+                print(amphipod_map[(r,c)], end="")
+            elif r == 1:
+                print('.', end="")
+            elif c in [3,5,7,9]:
+                print('.', end="")
+            else:
+                print(" ", end="")
+        print()
 
 # Amphipods will never stop on the space immediately outside any room.
 #   They can move into that space so long as they immediately continue moving.
@@ -83,11 +106,12 @@ def is_hallway(posn):
     r,_ = posn
     return r == 1
 
-def get_moves_to_hallway(amphipod_map, move_from):
+def get_moves_to_hallway(amphipod_map, move_from, depth):
     (r,c) = move_from
     # something is blocking on the way out, so return nothing
-    if r == 3 and (2,c) in amphipod_map:
-        return []
+    for d in range(r-1, 2-1,-1):
+        if (d,c) in amphipod_map:
+            return []
     
     letter = amphipod_map[move_from]
     moves = []
@@ -150,20 +174,31 @@ def get_move_into_room(amphipod_map, move_from):
 
     return (letter, move_from, (row, column))
 
-def get_moves(amphipod_map):
+def try_leaving_room(amphipod_map, c, depth) :
+    for current_depth in range(0, depth):
+        r = current_depth + 2
+        if (r,c) in amphipod_map and not amphipod_map[(r,c)] == cave_map[(r,c)]:
+            return get_moves_to_hallway(amphipod_map,(r,c), depth)
+        elif (r,c) in amphipod_map:
+            # matches, but is there anyone after that I need to move for?
+            all_match = True
+            for j in range(current_depth+1, depth):
+                rr = j + 2
+                if not amphipod_map[(rr,c)] == cave_map[(rr,c)]:
+                    all_match = False
+                    break
+            if not all_match or current_depth+1 == depth:
+                return get_moves_to_hallway(amphipod_map,(r,c), depth)
+    return []
+
+
+def get_moves(amphipod_map, depth):
     moves = []
     # moving out of the rooms
-    # at least one in the room is not correct, so we need to vacate them
     for c in [3,5,7,9]:
-        if not amphipod_map.get((2,c), '') == cave_map[(2,c)] or not amphipod_map[(3,c)] == cave_map[(3,c)]:
-            # the top guy must move if he's there
-            if (2,c) in amphipod_map:
-                moves.extend(get_moves_to_hallway(amphipod_map,(2,c)))
-            # the bottom guy moves if he's not already in the right location
-            #   and there is no one blocking him
-            if (3,c) in amphipod_map and not amphipod_map[(3,c)] == cave_map[(3,c)] and (2,c) not in amphipod_map:
-                moves.extend(get_moves_to_hallway(amphipod_map,(3,c)))
+        moves.extend(try_leaving_room(amphipod_map, c, depth))
 
+    # moving out of the hallway
     for c in [1,2,4,6,8,10,11]:
         # if some one is in the halway
         if (1,c) in amphipod_map:
@@ -172,7 +207,6 @@ def get_moves(amphipod_map):
                 moves.append(move)
 
 
-    # moving out of the hallway
     return moves
 
 def man_dist(a, b):
@@ -201,7 +235,7 @@ def freezemap(mutable_map):
     frozenmap.sort()
     return tuple(frozenmap)
 
-def min_energy(amphipod_map):
+def min_energy(amphipod_map, depth):
     priority_queue = []
     id = 0
     visited = {}
@@ -210,7 +244,7 @@ def min_energy(amphipod_map):
     heappush(priority_queue, (0, 0, amphipod_map.copy(), []))
     while len(priority_queue) > 0:
         current_energy, _, current_map, path = heappop(priority_queue)
-        for letter, move_from, move_to in get_moves(current_map):
+        for letter, move_from, move_to in get_moves(current_map, depth):
             additional_energy = energy(letter) * man_dist(move_from, move_to)
             next_energy = current_energy + additional_energy
             next_map = current_map.copy()
@@ -225,51 +259,64 @@ def min_energy(amphipod_map):
                 id = id + 1
                 heappush(priority_queue, (next_energy, id, next_map, new_path))
 
-    end_result = freezemap({
-        (2,3): 'A',
-        (3,3): 'A',
-        (2,5): 'B',
-        (3,5): 'B',
-        (2,7): 'C',
-        (3,7): 'C',
-        (2,9): 'D',
-        (3,9): 'D',
-    })
-    return (visited[end_result], paths[end_result])
+    end_result = {}
+    for i in range(0, depth):
+        end_result[(i+2,3)] = 'A'
+        end_result[(i+2,5)] = 'B'
+        end_result[(i+2,7)] = 'C'
+        end_result[(i+2,9)] = 'D'
+    end_result_frozen = freezemap(end_result)
+    return (visited[end_result_frozen], paths[end_result_frozen])
 
 
 
 
 print("expected: 12521")
-(me, path) = min_energy(sample)
+(me, path) = min_energy(sample, 2)
 print(f"actual:   {me}")
 
-def print_map(amphipod_map):
-    for r in range (1,3+1):
-        for c in range (1,11+1):
-            if (r,c) in amphipod_map:
-                print(amphipod_map[(r,c)], end="")
-            elif r == 1:
-                print('.', end="")
-            elif c in [3,5,7,9]:
-                print('.', end="")
-            else:
-                print(" ", end="")
-        print()
-
-def print_path(path, amphipod_map):
+def print_path(path, amphipod_map, depth):
     amphipod_map = amphipod_map.copy()
-    print_map(amphipod_map)
+    print_map(amphipod_map, 2)
     for step in path:
         print()
         print(str(step))
         letter, move_from, move_to, _, _ = step
         del amphipod_map[move_from]
         amphipod_map[move_to] = letter
-        print_map(amphipod_map)
+        print_map(amphipod_map, depth)
 
-print_path(path, sample)
+#print_path(path, sample, 2)
 
-print("puzzle input:")
-(me, path) = min_energy(input)
-print(me)
+#print("puzzle input:")
+#(me, path) = min_energy(input,2)
+#print(me)
+
+def unfold(amphipod_map):
+    return {
+        (2,3): amphipod_map[(2,3)],
+        (3,3): 'D',
+        (4,3): 'D',
+        (5,3): amphipod_map[(3,3)],
+        (2,5): amphipod_map[(2,5)],
+        (3,5): 'C',
+        (4,5): 'B',
+        (5,5): amphipod_map[(3,5)],
+        (2,7): amphipod_map[(2,7)],
+        (3,7): 'B',
+        (4,7): 'A',
+        (5,7): amphipod_map[(3,7)],
+        (2,9): amphipod_map[(2,9)],
+        (3,9): 'A',
+        (4,9): 'C',
+        (5,9): amphipod_map[(3,9)],
+    }
+
+unfolded_sample = unfold(sample)
+unfolded_input = unfold(input)
+
+print()
+print_map(unfolded_sample, 4)
+print()
+print_map(unfolded_input, 4)
+

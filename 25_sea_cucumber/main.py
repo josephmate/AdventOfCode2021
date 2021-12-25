ea05a83da755a6fbffde8e98598981281cf0732b
Joseph <mate.joseph@gmail.com>
2021-12-25 12:05:07 -0500

    simulating by steps complete

--- 25_sea_cucumber/main.py
+++ 25_sea_cucumber/main.py
@@ -17,13 +17,56 @@ def parse_file(filename):
     with open(filename) as reader:
         for line in reader:
             line = line.rstrip()
-            map.append(line)
+            map.append(list(line))
     return map
 
 sample1 = parse_file("sample1.txt")
 sample2 = parse_file("sample2.txt")
 input = parse_file("input.txt")
 
-print("\n".join(sample1))
+print("\n".join(map(lambda l: ''.join(l), sample1)))
 print()
-print("\n".join(sample2))
+print("\n".join(map(lambda l: ''.join(l), sample2)))
+
+
+
+def simulate_steps(map, steps):
+    for _ in range(0, steps):
+        next_map = copy.deepcopy(map)
+        # move east herd
+        for i in range(len(map)):
+            for j in range(len(map[i])):
+                if map[i][j] == '>' and map[i][(j+1)%len(map[i])] == '.':
+                    next_map[i][j] = '.'
+                    next_map[i][(j+1)%len(map[j])] = '>'
+        map = next_map
+        next_map = copy.deepcopy(map)
+        # move south herd
+        for i in range(len(map)):
+            for j in range(len(map[i])):
+                if map[i][j] == 'v' and map[(i+1)%len(map)][j] == '.':
+                    next_map[i][j] = '.'
+                    next_map[(i+1)%len(map)][j] = 'v'
+        map = next_map
+    return map
+
+
+
+
+print()
+expected = """>......
+..v....
+..>.v..
+.>.v...
+...>...
+.......
+v......"""
+print("Expected:")
+print(expected)
+print()
+actual = simulate_steps(sample1, 4)
+actual = "\n".join(map(lambda l: ''.join(l), actual))
+print("Actual:")
+print(actual)
+assert expected == actual
+
